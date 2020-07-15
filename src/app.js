51bd3b8f9f88b5cde1970bcbfcb9ba5ee7d3e1b1
julieritz <julie.ritz@gmail.com>
2020-07-15 12:32:06 -0700

    renaming files, adding intial index and app functionality

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Directory from "./components/Directory";
+
+function App() {
+    return (
+        <div>
+            <Directory />
+        </div>
+    );
+}
+
+export default App;
