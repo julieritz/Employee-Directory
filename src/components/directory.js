a89b20282bb1864143b01ff6e6ea89b37a9a1fe3
julieritz <julie.ritz@gmail.com>
2020-07-18 16:20:28 -0700

    getting functionality going

---
+++ src/components/directory.js
@@ -0,0 +1,64 @@
+import API from "../apiCall";
+import React, { Component } from "react";
+import Search from "./Search";
+import Employees from "./Employees";
+import "./stylesheet.css";
+
+class Directory extends Component {
+    state = {
+        search: "",
+        employees: [],
+        filtered: [],
+        order: ""
+    };
+
+componentDidMount() {
+    API.getUsers().then(res => this.setState({
+        employees: res.data.results,
+        filtered: res.data.results
+    })).catch(err => console.log(err))
+}
+
+handleInputChange = event => {
+    const employees = this.state.employees;
+    const input = event.target.value;
+    const filtered = employees.filter(employee => employee.name.first.toLowerCase().indexOf(input.toLowerCase()) > -1)
+    this.setState({
+        filtered,
+    });
+};
+
+nameSort = () => {
+    const filteredUsers = this.state.filtered;
+    if (this.state.order === "ascending") {
+        const sortedUsers = filteredUsers.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+        console.log(sortedUsers)
+        this.setState({
+            filtered: sortedUsers,
+            order: "descending"
+        })
+    } else {
+        const sortedUsers = filteredUsers.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1)
+        console.log(sortedUsers)
+        this.setState({
+            filtered: sortedUsers,
+            order: "ascending"
+        })
+    }
+}
+
+render() {
+    return (
+        <div>
+            <Search
+                employee={this.state.employees}
+                handleInputChange={this.handleInputChange} />
+            <Employees results={this.state.filtered}
+                nameSort={this.nameSort}
+            />
+        </div >
+        )
+    }
+}
+
+export default Directory
---
+++ src/components/employees.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Moment from "react-moment";
+import "./stylesheet.css";
+
+function Employees(props) {
+    return (
+        <div className="container-fluid">
+            <div className="row d-flex text-center">
+                <div className="col-11">
+                    <table className="table table-striped table-responsive-lg employees">
+                        <thead>
+                            <tr>
+                                <th>Photo</th>
+                                <th onClick={props.nameSort}>Name</th>
+                                <th>Phone Number</th>
+                                <th>E-mail</th>
+                                <th>Date of Birth</th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                            {props.results.map(result => (
+                                <tr className="table" key={result.login.uuid}>
+                                    <td> <img className="
+                        "src={result.picture.medium} alt="" /></td>
+                                    <td>{result.name.first + " " + result.name.last}  </td>
+                                    <td>{result.cell}</td>
+                                    <td className="email"><a href={result.email}>{result.email}</a></td>
+                                    <td><Moment format="MM/DD/YYYY">{result.dob.date}</Moment></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table >
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Employees
